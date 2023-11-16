f3c973df1f418cd60dbab612a1ae62818c76d47c
Serkan Sönmez <serkansonmzz@gmail.com>
2023-11-17 02:35:01 +0300

    Implement event handlers for dynamic navigation links in app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
+function handleDashboardLinkClick(event) {
+  const contentElement = document.querySelector("#content");
+  contentElement.innerHTML = renderDashboard();
+}
+function handleTeachersLinkClick(event) {
+  const contentElement = document.querySelector("#content");
+  contentElement.innerHTML = renderTeacherSection();
+}
+function handleStudentsLinkClick(event) {
+  const contentElement = document.querySelector("#content");
+  contentElement.innerHTML = renderStudentSection();
+}
+function handleClassesLinkClick(event) {
+  const contentElement = document.querySelector("#content");
+  contentElement.innerHTML = renderClassSection();
+}
+
 function initializeApp() {
   document.addEventListener("DOMContentLoaded", () => {
     const appRoot = document.querySelector("#app");
     appRoot.innerHTML = renderNavbar() + renderDashboard() + renderFooter();
+
+    //! management of event related to navbar links
+    document
+      .getElementById("dashboardLink")
+      .addEventListener("click", handleDashboardLinkClick);
+    document
+      .getElementById("teachersLink")
+      .addEventListener("click", handleTeachersLinkClick);
+    document
+      .getElementById("studentsLink")
+      .addEventListener("click", handleStudentsLinkClick);
+    document
+      .getElementById("classesLink")
+      .addEventListener("click", handleClassesLinkClick);
   });
 }
