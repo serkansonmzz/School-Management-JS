928513942c9dde58aaab5d2e9343e130d002e1d4
Serkan Sönmez <serkansonmzz@gmail.com>
2023-11-30 01:40:57 +0300

    added CRUD operations in services/classService.js

--- src/services/classService.js
+++ src/services/classService.js
@@ -11,3 +11,32 @@ const getClassById = (classId) => {
     (item) => item.type === "class" && item.id === Number(classId)
   );
 };
+
+const saveClass = (classInfo) => {
+  const schoolData = getFromLocalStorage("schoolData") || [];
+  schoolData.push({ ...classInfo, type: "class", id: Date.now() });
+  saveToLocalStorage("schoolData", schoolData);
+};
+
+const updateClass = (classId, updatedInfo) => {
+  const schoolData = getFromLocalStorage("schoolData") || [];
+
+  if (updatedInfo.students) {
+    updatedInfo.students = updatedInfo.students.map(Number);
+    updatedInfo.teacherId = Number(updatedInfo.teacherId);
+  }
+  schoolData = schoolData.map((item) => {
+    if (item.type === "class" && item.id === Number(classId)) {
+      return { ...item, ...updatedInfo };
+    }
+    return item;
+  });
+  saveToLocalStorage("schoolData", schoolData);
+};
+const deleteClass = (classId) => {
+  let schoolData = getFromLocalStorage("schoolData") || [];
+  schoolData = schoolData.filter(
+    (item) => !(item.type === "class" && item.id === Number(classId))
+  );
+  saveToLocalStorage("schoolData", schoolData);
+};
