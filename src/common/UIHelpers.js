3cff187dd7e8a4888ac7e1ce9644e796d566fbf0
Serkan Sönmez <serkansonmzz@gmail.com>
2023-12-13 12:53:51 +0300

    added new functions for alert operation

--- src/common/UIHelpers.js
+++ src/common/UIHelpers.js
@@ -18,4 +18,37 @@ function addLoadedClassToCards() {
     });
   }, 70);
 }
-export { makeActive, addLoadedClassToCards, customizeSelectBox };
+const showSweetAlert = (text, icon) => {
+  Swal.fire({
+    position: "middle",
+    icon: `${icon}`,
+    title: `${text}`,
+    showConfirmButton: false,
+    timer: 2000,
+  });
+};
+const showDeleteSweetAlert = (itemId, deleteFunction, refreshFunction) => {
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      deleteFunction(itemId);
+      Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      refreshFunction();
+    }
+  });
+};
+
+export {
+  makeActive,
+  addLoadedClassToCards,
+  customizeSelectBox,
+  showSweetAlert,
+  showDeleteSweetAlert,
+};
